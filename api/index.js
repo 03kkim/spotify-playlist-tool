542fe7cb02e4d935130b310ff2b7a2f1defff666
Kyu Hong Kim <03kkim@gmail.com>
2023-05-19 16:15:38 -0400

    Refactor entire codebase

--- api/index.js
+++ api/index.js
@@ -1,65 +1,47 @@
-// const express = require("express");
-// const app = express();
-// const cors = require("cors");
-// const dotenv = require("dotenv");
-// const querystring = require("querystring");
-// const axios = require("axios");
-
-// dotenv.config();
-
-// const port = 8080;
-// const client_id = process.env.CLIENT_ID;
-// const client_secret = process.env.CLIENT_SECRET;
-// const redirect_uri = "http://localhost:8080/callback";
-// var state = "";
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
-
-// const AuthRoutes = require("./routes/authRoutes.js");
-// app.use("/api", cors(), AuthRoutes);
-
-// // https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
-// function generateRandomString(length) {
-//   let result = "";
-//   const characters =
-//     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-//   const charactersLength = characters.length;
-//   let counter = 0;
-//   while (counter < length) {
-//     result += characters.charAt(Math.floor(Math.random() * charactersLength));
-//     counter += 1;
-//   }
-//   return result;
-// }
-
-// app.get("/test", (req, res) => {
-//   console.log("test called!");
-//   res.json("Success");
-// });
-
-// app.get("/", (req, res) => {
-//   res.json("Main page");
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server listening on the port::${port}`);
-// });
-
 const express = require("express");
 const cors = require("cors");
+const spotifyWebApi = require("spotify-web-api-node");
+
+const app = express();
+const port = 8000;
+
 require("dotenv").config();
-const PORT = process.env.PORT || 8080;
-const application = express();
 
-application.use(express.json());
-application.use(express.urlencoded({ extended: true }));
-application.use(cors());
+app.use(cors()); // To handle cross-origin requests
+app.use(express.json()); // To parse JSON bodies
 
-const AuthRoutes = require("./routes/authRoutes.js");
-application.use("/", cors(), AuthRoutes);
+const credentials = {
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: "http://localhost:3000/",
+};
 
-application.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+app.get("/", (req, res) => {
+  console.log("Hello World!");
+});
+
+app.post("/login", (req, res) => {
+  //  setup
+  let spotifyApi = new spotifyWebApi(credentials);
+
+  //  Get the "code" value posted from the client-side and get the user's accessToken from the spotify api
+  const code = req.body.code;
+
+  // Retrieve an access token
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      // Returning the User's AccessToken in the json formate
+      res.json({
+        accessToken: data.body.access_token,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
